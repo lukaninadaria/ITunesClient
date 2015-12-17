de3ae3528a84f567ff02bb7c1f6c534e8e845a66
lukaninadaria <lukaninadaria@gmail.com>
2015-12-17 16:28:48 +0200

    Removed cors related config

--- app/app.module.js
+++ app/app.module.js
@@ -1,15 +1,4 @@
 /**
  * Created by dashyki on 15.12.2015.
  */
-angular.module('app', ['ngRoute', 'ngResource'])
-    .config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common["X-Requested-With"];
-        $httpProvider.defaults.headers.common["Accept"] = "application/json";
-        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
-    }
-    ],
-    ['$resourceProvider', function($resourceProvider) {
-        // Don't strip trailing slashes from calculated URLs
-        $resourceProvider.defaults.stripTrailingSlashes = false;
-    }]);
+angular.module('app', ['ngRoute', 'ngResource']);
