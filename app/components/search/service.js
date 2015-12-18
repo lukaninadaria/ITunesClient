8f83ed95df3e7f015dbe4da3087b8de3950e250d
lukaninadaria <lukaninadaria@gmail.com>
2015-12-18 22:15:33 +0200

    Search service improvements. Changed grouping criteria for search results.

--- app/components/search/service.js
+++ app/components/search/service.js
@@ -1,31 +1,20 @@
 /**
  * Created by dashyki on 15.12.2015.
  */
-angular.module('app')
-    .service('SearchService', searchService);
+(function() {
+    angular.module('app')
+        .service('SearchService', ['ITunesSearchResult', searchService]);
 
-function searchService(ITunesSearchResult) {
-    this.search = function(keyWords, callback) {
-        //TODO replace
-        var self = this;
-        ITunesSearchResult.get({term : keyWords}, function(res) {
-            var results = res.hasOwnProperty('results')?self.parseResults(res):{};
-            callback && callback(results);
-        });
-    };
+    function searchService(ITunesSearchResult) {
+        this.search = function(keyWords, callback) {
+            ITunesSearchResult.get({term : keyWords}).$promise
+                .then(function(res) {
+                    callback && callback(parseResults(res));
+                });
+        };
 
-    this.parseResults = function(data) {
-        var resultJson = {};
-        //TODO shorten
-        if (data.hasOwnProperty('results')) {
-            for (var i = 0; i < data.resultCount; i++) {
-                var current = data.results[i];
-                if (!resultJson.hasOwnProperty(current.kind)) {
-                    resultJson[current.kind] = [];
-                }
-                resultJson[current.kind].push(current);
-            }
-        }
-        return resultJson;
+        function parseResults(data) {
+            return _.groupBy(data.results, 'wrapperType');
+        };
     };
-};
+})();
